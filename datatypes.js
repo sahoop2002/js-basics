0c9d29768bc93e25ac2c0fdb5c75d029f7d5568a
sahoop2002 <sahoopratip10@gmail.com>
2026-09-20 12:04:49 +0100

    initial commit

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,30 @@
-console.log("helloworld")
+"use strict"; // treat all JS code as newer version
+
+//  alert(3+3) // we are using node JS not browser
+console.log(3
+    +3
+)               // code readability slould be high 
+console.log(4+4)
+console.log("pratip")
+
+let name = "pratip"
+let age = 20
+let isLoggedIn = true
+ /*
+ number = 2 to power 53
+ bigint
+ string => " "
+ boolean => true/false  
+ null => standalone value
+ undefined =>
+ symbol => unique
+
+ object
+
+
+ */
+
+ console.log(typeof "pratip")
+
+ console. log (typeof undefined); // undefined 
+ console. log (typeof null); // object
