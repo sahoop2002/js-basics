604f8625ec6e24a842608b2c670adc1e76902380
sahoop2002 <sahoopratip10@gmail.com>
2026-09-20 12:00:55 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+// variables 
+const accountId = 144553
+let accountEmail = "pratip@gmail.com"
+var accountPassword = " 1443"
+accountCity = "kolkata"
+
+
+// accountId = 111 // not allowed \ TypeError: Assignment to constant variable.
+accountEmail = "ps@.com"
+accountPassword = "21212"
+accountCity = "assam"
+
+let accountState
+
+console.log(accountId);
+
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope 
+
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
