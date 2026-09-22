8bb4031b9f1c80222d4b634df5527577369d68d4
sahoop2002 <sahoopratip10@gmail.com>
2026-09-22 19:37:31 +0100

    initial commit

---
+++ 08_dates.js
@@ -0,0 +1,32 @@
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console. log (myCreatedDate.toDateString()) ;
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console. log (myCreatedDate.toLocaleString()) 
+
+let myCreatedDate1 = new Date("2023-01-14") 
+// console. log(myCreatedDate1.toLocaleString())
+
+let myTimeStamp = Date. now()
+// console. log (myTimeStamp);
+// console. log (myCreatedDate1.getTime());
+// console. log(Math.floor(Date. now()/1000));
+
+
+let newDate = new Date()
+console. log (newDate) ;
+console. log(newDate.getMonth() + 1);
+console. log (newDate.getDay()) ;
+
+// `${newDate.getDay()} and the time`
+
+newDate. toLocaleString(' default', {
+weekday: "long",
+})
