2ef37dd2c0110b291f6510d5f75188640307a41b
sahoop2002 <sahoopratip10@gmail.com>
2026-09-22 18:51:44 +0100

    initial commit

--- 07_nums_and_maths.js
+++ 07_nums_and_maths.js
@@ -1,31 +1,31 @@
-// const score = 400
-// console.log(score)
+const score = 400
+console.log(score)
 
 
-// const balance = new Number (100)
-// console. log (balance);
+const balance = new Number (100)
+console. log (balance);
 
-// console. log (balance.toString())
-// console. log (balance.toString(). length);
-// console. log (balance. toFixed (2));
+console. log (balance.toString())
+console. log (balance.toString(). length);
+console. log (balance. toFixed (2));
 
-// const otherNumber = 123.8966
-// console. log (otherNumber.toPrecision(4));
+const otherNumber = 123.8966
+console. log (otherNumber.toPrecision(4));
 
-// const hundreds = 1000000
-// console. log (hundreds. toLocaleString())
-// console. log (hundreds. toLocaleString('en-IN' ))
+const hundreds = 1000000
+console. log (hundreds. toLocaleString())
+console. log (hundreds. toLocaleString('en-IN' ))
 
 
-// // ------------------------------ Maths ----------------------------------
+// ------------------------------ Maths ----------------------------------
 
-// console.log (Math)
-// console. log (Math. abs (-4)); // = 4
-// console. log (Math. round (4.6)) // = 5
-// console. log(Math.ceil(4.2))   // = 5
-// console. log(Math. floor (4.9)) // = 4
-// console. log (Math. min (4, 3, 6, 8)) // = 3
-// console. log (Math. max (4, 3, 6, 8)) // = 8
+console.log (Math)
+console. log (Math. abs (-4)); // = 4
+console. log (Math. round (4.6)) // = 5
+console. log(Math.ceil(4.2))   // = 5
+console. log(Math. floor (4.9)) // = 4
+console. log (Math. min (4, 3, 6, 8)) // = 3
+console. log (Math. max (4, 3, 6, 8)) // = 8
 
 console. log (Math. random ()) // always give output in between 0 & 1
 console. log (( Math. random ()*10) + 1);
